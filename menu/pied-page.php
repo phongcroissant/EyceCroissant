a061aadf0da744b247eaf63e924f8280b5d27865
Phong Nguyen <nguyenp@SIOPERGAUD.FR>
2024-03-19 10:15:19 +0100

    bonjour

--- menu/pied-page.php
+++ menu/pied-page.php
@@ -6,7 +6,7 @@
 </head>
 <footer class="ps-5 pe-5 bg-danger-subtle pt-5 ">
     <div class="row">
-        <div class="col-md-3 mb-3 mx-auto">
+        <div class="col-md-12 col-lg-4 col-xl-3 mb-3 mx-auto">
             <h5>En savoir plus...</h5>
             <ul class="nav flex-column">
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Accueil</a></li>
@@ -17,7 +17,7 @@
             </ul>
         </div>
 
-        <div class="col-md-3 mb-3 mx-auto">
+        <div class="col-md-12 col-lg-4 col-xl-3 mb-3 mx-auto">
             <h5>Contact</h5>
             <ul class="nav flex-column">
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">5 avenue du parc</a></li>
@@ -27,13 +27,13 @@
                 <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">eycecroissant@contact.fr</a></li>
             </ul>
         </div>
-        <div class="col-md-3 mb-3 mx-auto">
+        <div class="col-md-12 col-lg-4 col-xl-3 mb-3 mx-auto">
             <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5419.863121539608!2d5.969635876721951!3d47.2179214147363!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x478d638b21756921%3A0x40458cc5650c33b8!2s5%20Av.%20du%20Parc%2C%2025000%20Besan%C3%A7on!5e0!3m2!1sfr!2sfr!4v1710315785152!5m2!1sfr!2sfr"
                     width="350" height="200" style="border:0;" allowfullscreen="" loading="lazy"
                     referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
 
-        <div class="col-md-3 mb-3">
+        <div class="col-md-12 col-lg-4 col-xl-3 mb-3">
             <form>
                 <h5>S'incrire à notre newsletter</h5>
                 <p>Soyez les premiers aux courants de nos nouveaux produits</p>
